9c9502eed7a2219f0ab9cfa99d474c1d3162e81b
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-06-02 12:36:06 +0200

    eh potions

---
+++ unity/cyber unity/Assets/Timme/zzzzexport/Items/PotionScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PotionScript : MonoBehaviour
+{
+    public GameObject player;
+    public int spinSpeed;
+    void OnTriggerEnter(Collider item)
+    {
+        if (item.gameObject.transform.tag == "Player")
+        {
+            if (player.GetComponent<ItemScript>().siringQuantity<3)
+            {
+                Invoke("Destroy", 1f);
+            }
+        }
+    }
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+    public void Spin()
+    {
+        transform.Rotate(0, 1, 0 * spinSpeed);
+    }
+    void Update()
+    {
+        Spin();
+    }
+}
---
+++ unity/cyber unity/Assets/Timme/zzzzexport/elevator/Elevator.cs
@@ -0,0 +1,82 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Elevator : MonoBehaviour
+{
+    public Animator anim;
+    public bool lightTrigger,buttonPressed,DeadBoss;
+    public int deurDichtGaanTijd;
+    public int knipperCount;
+    public int sceneInt;
+    public GameObject lightRed, LightWhite;
+    public GameObject hitBox;
+
+    private void Start()
+    {
+        lightRed.SetActive(false);
+        knipperCount = 0;
+        hitBox.SetActive(false);
+        lightTrigger = false;
+        buttonPressed = false;
+    }
+    public void Update()
+    {
+        if (DeadBoss == true)
+        {
+            anim.SetInteger("Condition", 1);
+        }
+        if (buttonPressed==true)
+        {
+            print("ff kijken");
+            lightTrigger = true;
+            anim.SetInteger("Condition", 2);
+            Invoke("ClosingDoors", 2f);
+        }
+        if (lightTrigger == true) {
+            Invoke("Flash", 3f);
+        }
+    }
+    public void ButtonPressed()
+    {
+        anim.SetInteger("Condition", 2);
+        Invoke("ClosingDoors", 0.2f);
+    }
+    public void ClosingDoors()
+    {
+        anim.SetInteger("Condition", 3);
+        buttonPressed = false;
+    }
+    public void Flash()
+    {
+        knipperCount++;
+        if (knipperCount <= deurDichtGaanTijd)
+        {
+            LightRood();
+           
+        }
+        else {
+            DoorClosed();
+        }
+    }
+
+    public void LightRood()
+    {
+        LightWhite.SetActive(false);
+        lightRed.SetActive(true);
+    }
+    public void LightWit()
+    {
+        LightWhite.SetActive(true);
+        lightRed.SetActive(false);
+    }
+    public void DoorClosed()
+    {
+        hitBox.SetActive(true);
+        LightWhite.SetActive(true);
+        lightRed.SetActive(false);
+        SceneManager.LoadScene(sceneInt);
+    }
+}
