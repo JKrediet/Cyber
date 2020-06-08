b3b43a40f6043da4d0d0f2821d90b0310e9bfd1f
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-06-08 12:33:17 +0200

    audio main menu als je over knopjes hoverd woahh

---
+++ unity/cyber unity/Assets/Scripts/AudioMainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMainMenu : MonoBehaviour
+{
+    public GameObject hoverSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayHover()
+    {
+        hoverSound.GetComponent<AudioSource>().Play();
+    }
+}
