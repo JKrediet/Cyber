6f209d3d8e184624481d4860a2c6ee4060554d61
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-06-09 02:17:38 +0200

    kijk desc
    folders gefixed zodat mike ze hopelijk mooi vind
    crocodildo kan je door heen als die dood gaat
    lift beetje aangepast
    geluid
    lvl 3 box gefixed
    lvl 3 coliders gegeven de meeste dingen

---
+++ unity/cyber unity/Assets/Scripts/Ui/PanelSwitcher.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PanelSwitcher : MonoBehaviour
+{
+    public GameObject rangedAttack, skillTree, escMenu, cameraUit, player;
+    public bool skillTreeActive, escMenuActive;
+    public Animator anim;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        cameraUit = GameObject.FindGameObjectWithTag("Camera");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Tab"))
+        {
+            if (skillTreeActive == false)
+            {
+                skillTreeActive = true;
+                Pauze();
+            }
+            else
+            {
+                skillTreeActive = false;
+                Resume();
+            }
+            SkillTreeOn();
+        }
+        if (Input.GetButtonDown("Escape"))
+        {
+            if (escMenuActive == false)
+            {
+                escMenuActive = true;
+                Pauze();
+            }
+            else
+            {
+                escMenuActive = false;
+                Resume();
+            }
+            EscMenuOn();
+        }
+    }
+    #region escMenuOn
+    public void EscMenuOn()
+    {
+        if (escMenuActive == true)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+
+            rangedAttack.SetActive(false);
+            skillTree.SetActive(false);
+            escMenu.SetActive(true);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            rangedAttack.SetActive(true);
+            skillTree.SetActive(false);
+            escMenu.SetActive(false);
+        }
+    }
+    #endregion
+    public void Pauze()
+    {
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        escMenuActive = false;
+        skillTreeActive = false;
+        EscMenuOn();
+    }
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void GetMeOuttaHere()
+    {
+        Application.Quit();
+    }
+    #region SkillTreeOn
+    public void SkillTreeOn()
+    {
+        if (skillTreeActive == true)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+
+            rangedAttack.SetActive(false);
+            skillTree.SetActive(true);
+            escMenu.SetActive(false);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            rangedAttack.SetActive(true);
+            skillTree.SetActive(false);
+            escMenu.SetActive(false);
+        }
+    }
+    #endregion
+}
