b207d6cefca42fccb4cd9a2d2b202110a2a9f144
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-06-09 14:59:55 +0200

    me push first!

--- unity/cyber unity/Assets/Scripts/Audio/AudioMC.cs
+++ unity/cyber unity/Assets/Scripts/Audio/AudioMC.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class AudioMC : MonoBehaviour
 {
-    public GameObject running;
+    public GameObject running, bow_aiming, melee_attack, gettingHit, bow_release, buttonPressed;
+
+    public void Sound_running()
+    {
+
+    }
+    public void Bow_aiming()
+    {
+
+    }
+    public void Melee_attack()
+    {
+
+    }
+    public void GettingHit()
+    {
+
+    }
+    public void Bow_release()
+    {
+
+    }
+    public void ButtonPressed()
+    {
+
+    }
 }
