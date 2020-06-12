4a0d97bb995faa4cc11e66405adb1ba6bad7e512
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-06-12 14:37:06 +0200

    restart overal een scriptje

---
+++ unity/cyber unity/Assets/Scripts/RestartOutsideMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartOutsideMap : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.gameObject.transform.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
